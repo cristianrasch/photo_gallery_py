b2f3dd460e77c63e428ace27992f7892f48ef72e
Cristian Rasch <cristianrasch@fastmail.fm>
2019-03-07 17:56:35 -0300

    Improve readability of int tests

--- tests/test_photo_gallery.py
+++ tests/test_photo_gallery.py
@@ -8,31 +8,36 @@ from photo_gallery.models import Picture
 
 @pytest.fixture
 def client():
+    pics_dir = str(Path(__file__).parent / 'fixtures')
     # the TESTING config flag is activated. What this does is disable error
     # catching during request handling, so that you get better error reports
     # when performing test requests against the application
-    pics_dir = str(Path(__file__).parent / 'fixtures')
     app = create_app({'TESTING': True, 'PICS_DIR': pics_dir})
-    client = app.test_client()
 
-    yield client
+    yield app.test_client()
 
 
 def test_get_root(client):
-    rv = client.get('/')
-    assert b'Oxford' in rv.data
-    assert b'Cambridge' in rv.data
+    res = client.get('/')
+
+    assert res.status_code == 200
+    assert b'Oxford' in res.data
+    assert b'Cambridge' in res.data
 
 
 def test_get_oxford(client):
-    rv = client.get('/Oxford')
+    res = client.get('/Oxford')
+
+    assert res.status_code == 200
     fname = b'p1060100_8840721601_o_opt.jpg'
-    assert Picture.WEB_SUBDIR.encode('utf-8') + b'/%s' % fname in rv.data
-    assert Picture.THUMB_SUBDIR.encode('utf-8') + b'/%s' % fname in rv.data
+    assert Picture.WEB_SUBDIR.encode('utf-8') + b'/%s' % fname in res.data
+    assert Picture.THUMB_SUBDIR.encode('utf-8') + b'/%s' % fname in res.data
 
 
 def test_get_cambridge(client):
-    rv = client.get('/Cambridge')
+    res = client.get('/Cambridge')
+
+    assert res.status_code == 200
     fname = b'p1060119_8831916022_o_opt.jpg'
-    assert Picture.WEB_SUBDIR.encode('utf-8') + b'/%s' % fname in rv.data
-    assert Picture.THUMB_SUBDIR.encode('utf-8') + b'/%s' % fname in rv.data
+    assert Picture.WEB_SUBDIR.encode('utf-8') + b'/%s' % fname in res.data
+    assert Picture.THUMB_SUBDIR.encode('utf-8') + b'/%s' % fname in res.data
